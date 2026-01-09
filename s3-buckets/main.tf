5188e2468d689b85ab78311f442934a3cae3773a
imaneeamo <imaneamokrane3@gmail.com>
2026-01-09 08:35:05 +0100

    Update main.tf

--- s3-buckets/main.tf
+++ s3-buckets/main.tf
@@ -2,11 +2,20 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# توليد لاحقة عشوائية
+resource "random_id" "suffix1" {
+  byte_length = 4
+}
+
+resource "random_id" "suffix2" {
+  byte_length = 4
+}
+
 resource "aws_s3_bucket" "bucket1" {
-  bucket = "aluruarumullaa1"
+  bucket = "imane-eks-bucket1-${random_id.suffix1.hex}"
 
   tags = {
-    Name        = "aluruarumullaa1"
+    Name        = "imane-eks-bucket1-${random_id.suffix1.hex}"
     Environment = "dev"
   }
 }
@@ -19,10 +28,10 @@ resource "aws_s3_bucket_versioning" "bucket1_versioning" {
 }
 
 resource "aws_s3_bucket" "bucket2" {
-  bucket = "arumullaaluruu1"
+  bucket = "imane-eks-bucket2-${random_id.suffix2.hex}"
 
   tags = {
-    Name        = "arumullaaluruu1"
+    Name        = "imane-eks-bucket2-${random_id.suffix2.hex}"
     Environment = "dev"
   }
 }
