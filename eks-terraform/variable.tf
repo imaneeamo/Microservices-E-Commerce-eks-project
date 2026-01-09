f7f1927068909ab7780d4f8546f18020c7cc40ca
imaneeamo <imaneamokrane3@gmail.com>
2026-01-09 10:33:20 +0100

    Update variable.tf

--- eks-terraform/variable.tf
+++ eks-terraform/variable.tf
@@ -1,3 +1,15 @@
+variable "cluster_role_arn" {
+  description = "IAM Role ARN for EKS Cluster"
+  type        = string
+  default     = "arn:aws:iam::730335625185:role/labrole"
+}
+
+variable "worker_role_arn" {
+  description = "IAM Role ARN for EKS Worker Nodes"
+  type        = string
+  default     = "arn:aws:iam::730335625185:role/labrole"
+}
+
 variable "node_group_name" {
   description = "Name of the EKS node group"
   type        = string
